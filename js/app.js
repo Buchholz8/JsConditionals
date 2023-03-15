822b6f3a5c0f7d2580ebc6db27671bca2d751aa0
markb <markbuchholz8@gmail.com>
2023-03-14 19:50:39 -0600

    esle

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+let userage=18;
+if(userage>=18){
+    console.log("")
+}
 let user_points = 70;
 if (user_points >= 75){
     console.log("Congrates")
 }else{
-    console.log("you have made no points")
+    console.log("you do not have any points")
 }
