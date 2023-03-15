dd7019b43421c2010a2fbc1899f4b9200d55d79d
markb <markbuchholz8@gmail.com>
2023-03-14 20:44:17 -0600

    is_member

--- js/app.js
+++ js/app.js
@@ -2,9 +2,17 @@ let userage=18;
 if(userage>=18){
     console.log("")
 }
-let user_points = 70;
+let user_points = 5;
+let is_member = true;
 if (user_points >= 75){
     console.log("Congrates")
 }else{
     console.log("you do not have any points")
+}
+
+if(is_member === true && user_points >=90){
+    console.log("you did it, congrates")
+}
+if(is_member === false || user_points < 10){
+console.log("Please, aquire more points")
 }
